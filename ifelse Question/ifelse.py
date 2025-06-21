5e94e62982c01cf470361cdd0ce0c24c189459aa
Aditya gandharw <aadigandharw22@gmail.com>
2025-06-21 17:04:14 +0530

    Ifelse Questions Practice

---
+++ ifelse Question/ifelse.py
@@ -0,0 +1,86 @@
+# Task-1 Check if a number is positive, negative, or zero.
+
+# print('Task-1 Check if a number is positive, negative, or zero.')
+
+# a =int(input("Enter Your Number:"))
+
+# if(a==0):
+#     print("Your Number Is 0")
+
+# elif(a>0):
+#     print("Your Number Is Positive")
+# else:
+#     print("Your Number Is Negative")
+
+
+# Task-2 Check Your Number Is Even Or Odd
+# print("Task-2 Check Your Number Is Even Or Odd.")
+
+# Value = int(input("Enter Your Value:"))
+
+# if (Value%2==0):
+#     print("Your Number Is Even!!!")
+# else:
+#     print("Your Number Is Odd!!!")
+
+
+
+
+# Task-3 Find the largest among three numbers.
+
+# print("Task-3:Find the largest among three numbers.")
+
+# number1=int(input("Enter Your First Number :"))
+# number2=int(input("Enter Your Second Number :"))
+# number3=int(input("Enter Your Third Number :"))
+
+# if (number1 == number2 == number3):
+#     print("You Enter Equal Same Number 3 times ",number3)
+
+# elif(number1>number2 and number1>number3):
+#     print(f"Your First Number {number1} Is Greater")
+
+# elif(number2>number1 and number2>number3):
+#     print(f"Your Second Number {number2} Is Greater")
+
+# else:
+#     print(f"Your Third Number {number3} Is Greater")
+
+# Task 4 :Check whether a year is a leap year.
+
+# Year=int(input("Enter Year:"))
+
+# if (Year%4 ==0 and (Year%400==0 or Year%100 !=0)):
+#     print(f"Your Year {Year} is Leap Year")
+
+# else :
+#     print("Year Is Not A Leap Year")
+
+
+
+
+
+
+# Task-5 :Check if a character is a vowel or consonant.
+
+# Character = input("Enter A Character:")
+# if ('a'==Character  or 'e' ==Character or 'i' ==Character or 'o'==Character or  'u'==Character or 'A'==Character or 'I' ==Character or 'E' ==Character or  'O'==Character or 'U'==Character):
+#     print("Your Letter Is Vowels")
+
+# else:
+#     print("Your Letter Is Consonant")
+
+
+# Task-5 :Check if a person is eligible to vote.
+
+# Age = int(input("Enter Your Age :"))
+# if (Age<0):
+#     print("Age Never be Negative!!!!")
+#     print("Please EnterValid Age")
+# elif (Age<18):
+#     print("You Can't Vote Wait Few Year")
+#     print("Thank You")
+# else:
+#     print("Your Can Vote")
+#     print("Thank You")
+
