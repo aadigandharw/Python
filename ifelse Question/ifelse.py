c8f62d9af245b0defdb77c7876bc1447015da22e
Aditya gandharw <aadigandharw22@gmail.com>
2025-06-23 21:22:47 +0530

    If Else Practice Question Is added

--- ifelse Question/ifelse.py
+++ ifelse Question/ifelse.py
@@ -26,6 +26,8 @@
 
 
 
+
+
 # Task-3 Find the largest among three numbers.
 
 # print("Task-3:Find the largest among three numbers.")
@@ -71,6 +73,7 @@
 #     print("Your Letter Is Consonant")
 
 
+
 # Task-5 :Check if a person is eligible to vote.
 
 # Age = int(input("Enter Your Age :"))
@@ -84,4 +87,76 @@
 #     print("Your Can Vote")
 #     print("Thank You")
 
-# Task 6  This Is For Percentage Based Condition
+
+# Task 6  Find grade based on percentage.
+# percentage = int(input("Enter Your Percentage :"))
+
+# if(percentage >100):
+#     print("Invalid Percentage");
+# elif(percentage >= 90 and percentage <100):
+#     print("A++ Grade")
+# elif(percentage >=75  and percentage <80):
+#     print("A Grade")
+# elif(percentage >= 60 and percentage <75):
+#     print("B Grade")
+# elif(percentage >= 45 and percentage <60):
+#     print("C Grade") 
+# elif(percentage > 33 and percentage <45):
+#     print("D Grade")  
+# else:
+#     print("You Failed")
+
+
+
+# Task-7 Check if a number is divisible by 5 and 11.
+
+# number = int(input("Enter Your Number :"))
+
+# if (number % 5 == 0 and number % 11== 0):
+#     print("Your Number Is divisible By 5 and 11")
+
+# else :
+#     print("Your Number Is Not Divisible By 5 and 11")
+
+# Task-8 Check if the angles form a valid triangle.
+
+# Angel1 = int(input("Enter Your first Angel: "))
+# Angel2 = int(input("Enter Your Second Angel: "))
+# Angel3 = int(input("Enter Your Third Angel: "))
+
+# if (Angel1 + Angel2 + Angel3 == 180):
+#     print("Your Angels Are valid!!!")
+
+# else:
+#     print("Your Angel Are Invalid!!!")
+
+
+#Task -9 Check if a number is an Armstrong number.Using While Loop;
+
+
+# number = int(input("Enter The Value :"))
+
+# count = len(str(number))
+# print("count =",count)
+# ans= 0;
+# while(number != 0):
+#     rem = number%10;
+#     number = number//10;
+#     ans = ans + (rem**count);
+
+# print("The Value ",ans);
+
+# Using For Loop
+
+number = int (input("Enter A Number : "))
+str_number = str(number)
+count = len(str_number)
+r= 0;
+for i in str_number:
+    r = r + (int(i) ** count);
+
+print(r);
+
+
+
+
