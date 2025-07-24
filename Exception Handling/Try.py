3b1bf850afec660be87c32069c3a17967effdefb
Aditya gandharw <aadigandharw22@gmail.com>
2025-07-24 12:59:02 +0530

    New File Is Added

--- Exception Handling/Try.py
+++ Exception Handling/Try.py
@@ -1,3 +1,5 @@
+# Example No 1 
+
 try:
     x = 10
     print("Inside The Block")
@@ -5,4 +7,18 @@ try:
 
 except:
     print("Inside The Except Block")
-    
+
+
+# Example No 2
+
+try: 
+    num = 120 
+    l=[2,5,6,'a',0] 
+    for i in l: 
+        print(num//i) 
+except ZeroDivisionError: 
+    print('ZeroDivision error occured') 
+except IndexError: 
+    print('index error occured') 
+except: 
+    print('unknown error occured')
